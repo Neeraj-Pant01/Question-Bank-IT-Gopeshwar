a9cada846a09f8e80c7fe1ba992a19aaa4970fb4
Neeraj Pant <105204939+Neeraj-Pant01@users.noreply.github.com>
2022-12-27 19:55:47 +0530

    Add files via upload

---
+++ src/dataSection/SecondYear.js
@@ -0,0 +1,65 @@
+
+const SecondYearCse = [
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+]
+
+const SecondYearMe = [
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+]
+
+const SecondYearCe = [
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+]
+const SecondYearEce = [
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+]
+const SecondYearEe = [
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+]
+
+export {SecondYearCse, SecondYearMe , SecondYearCe, SecondYearEce, SecondYearEe}
---
+++ src/dataSection/ThirdYear.js
@@ -0,0 +1,66 @@
+const ThirdYearCse =[
+    {sub:"DAA",link:"/" },
+    {sub:"Computer Networks",link:"/" },
+    {sub:"JAVA Programming",link:"/" },
+    {sub:"Principles of Programming Languages",link:"/" },
+    {sub:"Operating System",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+]
+
+const ThirdYearCe =[
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+]
+
+const ThirdYearEce =[
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+]
+
+const ThirdYearEe =[
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+]
+
+const ThirdYearMe =[
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+    {sub:"sub1",link:"/" },
+]
+
+export {ThirdYearCe, ThirdYearCse, ThirdYearMe, ThirdYearEe, ThirdYearEce}
